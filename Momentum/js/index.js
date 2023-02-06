b12298cb2a77f90cb49db0b60a5b6a5f5444cd5e
Alexandr Dimkov <dimkovaleks4@gmail.com>
2023-02-06 20:16:25 +0300

    fead: add clock and calendar

--- Momentum/js/index.js
+++ Momentum/js/index.js
@@ -1,4 +1,24 @@
+// Часы и календарь
 
+const time = document.querySelector(".time");
+const day = document.querySelector(".day");
+const options = {weekday: 'long', month: 'long', day: 'numeric', timeZone: 'UTC'};
+
+const showDay = () => {
+    const date = new Date();
+    const currentDay = date.toLocaleDateString('ru-RU', options);
+    day.textContent = currentDay;
+}
+
+const showTime = () => {
+    const date = new Date();
+    const currentTime = date.toLocaleTimeString();
+    time.textContent = currentTime.toUpperCase();
+    showDay()
+    setTimeout(showTime, 1000);
+}
+
+showTime();
 
 
 
