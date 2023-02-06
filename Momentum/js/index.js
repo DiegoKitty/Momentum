2d491feb164c4fa391c708d16e45c963749fc0df
Alexandr Dimkov <dimkovaleks4@gmail.com>
2023-02-06 23:07:10 +0300

    feat: add slider

--- Momentum/js/index.js
+++ Momentum/js/index.js
@@ -13,7 +13,7 @@ const showDay = () => {
 const showTime = () => {
     const date = new Date();
     const currentTime = date.toLocaleTimeString();
-    time.textContent = currentTime.toUpperCase();
+    time.textContent = currentTime;
     showDay()
     setTimeout(showTime, 1000);
 }
@@ -21,6 +21,7 @@ const showTime = () => {
 showTime();
 
 // Часы и календарь
+
 
 // Приветствие
 
@@ -38,6 +39,7 @@ const showGreeting = () => {
     const hours = date.getHours();
     greeting.textContent = `Good ${greetings[Math.floor(hours / 6)]},`;
     setTimeout(showGreeting, 1000);
+    return greetings[Math.floor(hours / 6)];
 }
 
 showGreeting ();
@@ -59,6 +61,61 @@ function getLocalStorage() {
 window.addEventListener('load', getLocalStorage);
 
 // Приветствие
+
+
+// Слайдер
+
+const slideNext = document.querySelector(".slide-next");
+const slidePrev = document.querySelector(".slide-prev");
+
+// Загружать рандомный фон при загрузке
+
+const getRandomNum = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+let randomNum = getRandomNum(1, 20);
+
+const setBg = () => {
+    const greeting = showGreeting().toLowerCase();
+    const bgNum = randomNum.toString().padStart(2, "0");
+    const img = new Image();
+    getRandomNum(1, 20);
+
+    img.src = `https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${greeting}/${bgNum}.jpg`;
+
+    img.onload = () => {      
+        document.body.style.backgroundImage = `url(${img.src})`
+      }; 
+}
+
+setBg();
+
+// Менять фон при клике на слайдер
+
+const getSlideNext = () => {
+    randomNum += 1;
+  if (randomNum === 21) {
+    randomNum = 1;
+  }
+      setBg();
+}
+
+const getSlidePrev = () => {
+    randomNum -= 1;
+  if (randomNum === 1) {
+    randomNum = 20;
+  }
+      setBg();
+}
+
+slideNext.addEventListener('click', getSlideNext);
+slidePrev.addEventListener('click', getSlidePrev);
+
+// Слайдер
+ 
 
 
 
